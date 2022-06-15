f79d63271321e6768604a6200ee69fbc44fbd2d7
Arun Jacob <arunjacob2@gmail.com>
2022-06-15 00:54:17 -0400

    Add snackbar when user logs in successfully

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles.css";
 import Styles from "../styles/HomePage.module.css";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { UserCard } from "./UserCard";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 export default function LoginForm() {
+  const [open, setOpen] = useState(false);
+
   const navigate = useNavigate();
   const loginStatusState = useSelector((state) => state.LoginStatusReducer);
   //logout
@@ -14,17 +25,33 @@ export default function LoginForm() {
     logout();
   };
   //logut end
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
   // const {user,isAuthenticated} =useAuth0();
   useEffect(() => {
     if (!isAuthenticated) navigate("/login");
+    setOpen(true);
   }, []);
 
   return (
-    isAuthenticated && (
-      <div className={Styles.HomePage}>
-        <UserCard user={user}></UserCard>
-        Hello {user.name}! Welcome
-      </div>
-    )
+    <>
+      {isAuthenticated && (
+        <div className={Styles.HomePage}>
+          <UserCard user={user}></UserCard>
+          Hello {user.name}! Welcome
+        </div>
+      )}
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Welcome {user?.name}.
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
